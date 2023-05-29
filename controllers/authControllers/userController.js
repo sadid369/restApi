34812bef584b914bb3cd3f25deefe5f1119b2b90
Sadid <sadid.jones@gmail.com>
2023-05-29 20:17:11 +0600

    @1:56

--- controllers/authControllers/userController.js
+++ controllers/authControllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../../models/users");
+const bcrypt = require("bcrypt");
 exports.getAllUser = async (req, res, next) => {
   try {
     const user = await User.find();
@@ -6,5 +7,37 @@ exports.getAllUser = async (req, res, next) => {
     res.status(200).json(user);
   } catch (error) {
     res.status(401).json({ message: "Something went wrong!" });
+  }
+};
+
+//update Profile
+exports.updateUser = async (req, res, next) => {
+  const userId = req.params.userId;
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(400).json({ message: "User Not Found" });
+    }
+    req.body.password = await bcrypt.hash(req.body.password, 11);
+    const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+      new: true,
+    });
+    res.status(200).json({ message: "User Updated Successfully", updatedUser });
+  } catch (error) {
+    res.status(401).json({ message: "You van't Updated This Account" });
+  }
+};
+exports.deleteUser = async (req, res, next) => {
+  const userId = req.params.userId;
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(400).json({ message: "User Not Found" });
+    }
+
+    const deletedUser = await User.findByIdAndDelete(userId);
+    res.status(200).json({ message: "User Deleted Successfully", deletedUser });
+  } catch (error) {
+    res.status(401).json({ message: "You can't Deleted This Account" });
   }
 };
--- routes/auth/userRouter.js
+++ routes/auth/userRouter.js
@@ -1,10 +1,14 @@
 const {
   getAllUser,
+  updateUser,
+  deleteUser,
 } = require("../../controllers/authControllers/userController");
 const { authMiddleware } = require("../../middlewares/auth");
 
 const userRouter = require("express").Router();
 
 userRouter.get("/", authMiddleware, getAllUser);
+userRouter.put("/:userId", authMiddleware, updateUser);
+userRouter.delete("/:userId", authMiddleware, deleteUser);
 
 module.exports = userRouter;
